3b54e3a90787941186fc36ce0e753b23f972f08b
JanhaviShigwan <janhavi.shigwan@somaiya.edu>
2026-02-23 23:12:38 +0530

    Phone Error Solved

--- Frontend/eventsphere/src/components/Footer.jsx
+++ Frontend/eventsphere/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Mail, Phone, MapPin, Github, Linkedin, Instagram } from "lucide-react";
+import { Mail, MapPin, Github, Linkedin, Instagram } from "lucide-react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logoo.png";
 import "../styles/footer.css";
